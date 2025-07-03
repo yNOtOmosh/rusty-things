2fa5b9583e85eee7118858738f39711482c7bb15
yNOtOmosh <onyango.omondi.tony@gmail.com>
2025-07-03 22:24:45 +0300

    variables and immutability

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
+/* fn main() {
     println!("Hello, world!");
+}*/
+
+fn main() {
+    let x = 5; //immutable
+    let mut y = 10; //mutable
+
+    println!("x = {}, y ={}", x, y);
+
+    y += 5; // allowed cz y is mutable
+    println!("Now y = {}", y);
 }
