41faf839f82f2053ec69b187c579f939822e32f5
yNOtOmosh <onyango.omondi.tony@gmail.com>
2025-07-22 22:39:45 +0300

    basic syntax of functions

---
+++ Functions/basic_syntax/src/main.rs
@@ -0,0 +1,11 @@
+fn add(a: i32, b:i32) -> i32 {
+    return a + b;
+}
+// fn = function
+// a: i32 = parameter with type
+// -> i32 = return type
+// return is optional if i endwith an expression
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b // no semicolon = implicit return
+}
---
+++ Functions/basic_syntax/target/debug/deps/basic_syntax.d
@@ -0,0 +1,5 @@
+C:\Users\tonyn\Desktop\rusty-things\Functions\basic_syntax\target\debug\deps\basic_syntax.exe: src\main.rs
+
+C:\Users\tonyn\Desktop\rusty-things\Functions\basic_syntax\target\debug\deps\basic_syntax.d: src\main.rs
+
+src\main.rs:
