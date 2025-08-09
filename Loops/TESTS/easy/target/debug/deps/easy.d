b0194bc6c4efd633b8993c29f168ce194738f098
yNOtOmosh <onyango.omondi.tony@gmail.com>
2025-08-09 17:13:45 +0300

    easy prctice

---
+++ Loops/TESTS/easy/target/debug/deps/easy.d
@@ -0,0 +1,5 @@
+C:\Users\tonyn\Desktop\rusty-things\Loops\TESTS\easy\target\debug\deps\easy.exe: src\main.rs
+
+C:\Users\tonyn\Desktop\rusty-things\Loops\TESTS\easy\target\debug\deps\easy.d: src\main.rs
+
+src\main.rs:
---
+++ Loops/TESTS/easy/target/debug/easy.d
@@ -0,0 +1,1 @@
+C:\Users\tonyn\Desktop\rusty-things\Loops\TESTS\easy\target\debug\easy.exe: C:\Users\tonyn\Desktop\rusty-things\Loops\TESTS\easy\src\main.rs
