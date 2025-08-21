0ae14f204f037b67e264a983b090cb0edf14d59d
yNOtOmosh <onyango.omondi.tony@gmail.com>
2025-08-21 20:13:02 +0300

    creating the display

--- to-do-project/to-do-cli/src/main.rs
+++ to-do-project/to-do-cli/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Write};
+
+struct Task {
+    description: String,
+    completed: bool,
 }
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut tasks: Vec<Task> = Vec::new();
+
+    loop {
+        println!("\n** to- do **");
+        println!("1. Add Task");
+        println!("2. View Tasks");
+        println!("3. Remove Tasks");
+        println!("4. Quit");
+        print!("> ");
+        io::stdout().flush()?;
+
+        let mut choice = String::new();
+        io::stdin().read_line(&mut choice)?;
+        match choice.trim() {
+            "1" => add_task(&mut tasks)?,
+            "2" => list_tasks(&tasks)?,
+            "3" => remove_task(&mut tasks)?,
+            "4" => break,
+            _ => eprintln!("Invalid choice"),
+        }
+    }
+
+    Ok(())
+}
+
+
---
+++ to-do-project/to-do-cli/target/debug/deps/to_do_cli.d
@@ -0,0 +1,5 @@
+F:\rusty-things\to-do-project\to-do-cli\target\debug\deps\to_do_cli.exe: src\main.rs
+
+F:\rusty-things\to-do-project\to-do-cli\target\debug\deps\to_do_cli.d: src\main.rs
+
+src\main.rs:
